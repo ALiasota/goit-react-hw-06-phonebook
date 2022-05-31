6ccf58c03f1bd76ccf865916353ffa811990298d
Andrii <Andrii@admins-MacBook-Pro.local>
2022-05-31 19:56:50 +0300

    g

---
+++ src/redux/contacts/contacts-selectors.js
@@ -0,0 +1,14 @@
+export const getFilter = state => state.contacts.filter;
+export const getContacts = state => state.contacts.contacts;
+
+export const getFilteredContacts = state => {
+  const contacts = getContacts(state);
+  return contacts;
+  //   const normalizeFilter = getFilter(state).toLowerCase();
+  //   const contacts = getContacts(state);
+  //   console.log(contacts);
+
+  //   return contacts.filter(({ name }) =>
+  //     name.toLowerCase().includes(normalizeFilter)
+  //   );
+};
